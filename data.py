66ca46b70ffa313c9b87262db7c525aac63fa930
lee-anne-jpg <chan.min.en.lee-anne_2026@nyjc.edu.sg>
2026-08-21 11:58:16 +0800

    ghg

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
+def mud.welcome():
+    """Displays the welcome screen when the player starts the game."""
+    print("welcome")
 
+def game.showroomdesc():
+    """Displays the screen when the player enters the foyer."""
+    print(f"{room} ({level})")
+    print(f"{room desc}")
+    print(f"{choices}") 
+    def mud.get_options():
+        """Takes the player's input and returns the corresponding action."""
+        
