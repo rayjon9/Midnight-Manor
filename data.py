6f6355279740d7b8ee67ad63d3a944dfdace1eee
lee-anne <chan.min.en.lee-anne_2026@nyjc.edu.sg>
2026-08-24 12:41:54 +0800

    display

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from game import Player
+from game import Player, Room, 
 
 def create_player():
     """Creates a new player object."""
@@ -15,7 +15,15 @@ def create_room():
     room.set_layout(layout)
     return room
     
-def display(status):
+def display(status: dict):
     """Displays the current game status."""
-    print(f"Current Status: {status}")
-
+    print("============================================================")
+    print(status["room_name"])
+    print("============================================================")
+    print(status["item_description"])
+    print(status["enemy_description"])
+    print("------------------------------------------------------------")
+    for i, option in enumerate(status["options"], start=1):
+        print(f"{i} - {option}")
+    print("------------------------------------------------------------")
+ 
