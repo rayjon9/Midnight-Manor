91639f3b7726ee32b22848644f71d4c0fd52dc90
codednyjc <chia.hao.lin.elgin_2026@nyjc.edu.sg>
2026-08-24 11:37:16 +0800

    Refactor Room and Player classes
    Refactor Room and Player classes for improved functionality and structure. Update methods for room layout, player inventory, and movement options.

--- game.py
+++ game.py
@@ -6,44 +6,84 @@
 #     def show_room(room):
 #         print(room)
 #     def show_player_options():
+game = True
 
 class Room:
-    def __init__(self):
-        self.layout = []
-    def set_layout(self, layout):
+    def __init__(self, layout):
         self.layout = layout
+        self.next_room = None
     def get_layout(self):
         print(self.layout)
     def set_starting_point(self, x, y):
         self.spawn_x = x
         self.spawn_y = y
-    def set_exit(self, x, y):
-        self.exit_x = x
-        self.exit_y = y
     def search_grid(self, x, y):
+        if x < 0 or x > 7 or y < 0 or y > 7:
+             return "M"
         return self.layout[y][x]
-        
+    def change(self):
+        current_room = self.next_room
+        print(current_room)
+    
 class Player:
-    def __init__(self, x, y):
+    def __init__(self):
         self.x = None
         self.y = None
+        self.inv = []
     def spawn(self, x, y):
         self.x = x
         self.y = y
-    def query(self, tile):
-        if tile = "M":
-            break
-        if tile = "."
     def get_coords(self):
         print(self.x, self.y)
+    def append_inv(self, item):
+        self.inv.append(item)
+    def get_inv(self):
+        for item in self.inv:
+            print(item)
     def choice(self):
         dcs = input("WASD - Move, E - Open Inventory")
         if dcs.upper() == "W":
+            if current_room.search_grid(self.x, self.y - 1) == ".":
+                current_room.layout[self.y][self.x] = "."
+                self.y -= 1
+                current_room.layout[self.y][self.x] = "X"
+            elif current_room.search_grid(self.x, self.y - 1) == "E":
+                current_room.change()
+            else:
+                print("Move not available!")
+        elif dcs.upper() == "A":
+            if current_room.search_grid(self.x - 1, self.y) == ".":
+                current_room.layout[self.y][self.x] = "."
+                self.x -= 1
+                current_room.layout[self.y][self.x] = "X"
+            elif current_room.search_grid(self.x - 1, self.y) == "E":
+                current_room.change()
+            else:
+                print("Move not available!")
+        elif dcs.upper() == "S":
+            if current_room.search_grid(self.x, self.y + 1) == ".":
+                current_room.layout[self.y][self.x] = "."
+                self.y += 1
+                current_room.layout[self.y][self.x] = "X"
+            elif current_room.search_grid(self.x, self.y + 1) == "E":
+                current_room.change()
+            else:
+                print("Move not available!")
+        elif dcs.upper() == "D":
+            if current_room.search_grid(self.x + 1, self.y) == ".":
+                current_room.layout[self.y][self.x] = "."
+                self.x += 1
+                current_room.layout[self.y][self.x] = "X"
+            elif current_room.search_grid(self.x + 1, self.y) == "E":
+                current_room.change()
+            else:
+                print("Move not available!")
+        elif dcs.upper() == "E":
             
-            self.y += 1
+        else:
+            print("Choice does not exist!")
 
-foyer = Room()
-foyer.set_layout([["M","M","M","M","M","M","M","M"],
+foyer = Room([["M","M","M","M","M","M","M","M"],
         ["M","M","M","M","M","M","M","M"],
         ["M","M","M","M","M","M","M","M"],
         [".",".",".",".",".",".",".","."],
@@ -51,13 +91,19 @@ foyer.set_layout([["M","M","M","M","M","M","M","M"],
         [".",".",".",".",".",".",".","."],
         ["M","M","M","M","M","M","M","M"],
         ["M","M","M","M","M","M","M","M"]])
-foyer.get_layout()
 foyer.set_starting_point(0, 4)
-foyer.set_exit(7, 4)
+living = Room([["M","M","M","E","E","M","M","M"],
+        [".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".","."],
+        ["X",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".","."],
+        ["M","M","M","M","M","M","M","M"]])
+living.set_starting_point(0, 4)
+foyer.next_room = living
 current_room = foyer
 
-player = Player(None,None)
+player = Player()
 player.spawn(current_room.spawn_x, current_room.spawn_y)
-player.get_coords()
-
-player.query(foyer.search_grid(player.get_coords()))
+#player.get_coords()
