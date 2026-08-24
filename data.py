3fcbfed401af7ac8cfe1c046b432a6d650b015ee
lee-anne <chan.min.en.lee-anne_2026@nyjc.edu.sg>
2026-08-24 12:54:08 +0800

    room

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from game import Player, Room, 
+from game import Player, Room
 
 def create_player():
     """Creates a new player object."""
@@ -7,10 +7,11 @@ def create_player():
 
 def create_room():
     """Creates a new room object."""
-    layout = []
-    for i in range(8):
-        row = input(f"Enter row {i+1} of the room layout (use '.' for empty space and 'M' for walls): ")
-        layout.append(list(row))
+    layout = room.txt
+    with open("room.txt", "r") as f:
+        layout = []
+        for line in f:
+            layout.append(line.strip().split())
     room = Room()
     room.set_layout(layout)
     return room
@@ -26,4 +27,3 @@ def display(status: dict):
     for i, option in enumerate(status["options"], start=1):
         print(f"{i} - {option}")
     print("------------------------------------------------------------")
- 
